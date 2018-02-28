8e5e1581667ab1bb5e04633170a33779856ecb24
Bob7410852 <36932431+Bob7410852@users.noreply.github.com>
2018-02-28 10:52:44 -0800

    Add files via upload

---
+++ Monster Mouth Adam.js
@@ -0,0 +1,26 @@
+var x = 0;
+var y = 0;
+function setup() {
+    createCanvas(450,350);
+    background(244,0,14);
+}
+
+function draw(){
+    
+    while (x < 450) {
+    x = x + 48;
+    rect(x,0,25,25);
+}
+        
+    while (y < 500) {
+    y = y + 48;
+    rect(y,330,25,25);
+}
+{
+    rect(188,80,100,250,45);
+    fill(0,255,0);
+}
+    rect(188,79,100,50);
+    fill(0,255,0);
+}
+    
